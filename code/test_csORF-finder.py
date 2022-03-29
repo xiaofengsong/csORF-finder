bc986316479c046f9a6777f49ada9d0a5cd67bb2
zhaodoctor <zhaodoctor@users.noreply.github.com>
2022-03-29 17:48:53 +0800

    Update test_csORF-finder.py

--- code/test_csORF-finder.py
+++ code/test_csORF-finder.py
@@ -10,7 +10,13 @@ parse.add_option('-f','--input',dest = 'inputfile',action = 'store',metavar = 'i
 parse.add_option('-o','--output',dest = 'outputpath',action = 'store',metavar = 'output file path',help = 'Please enter output file name')
 parse.add_option('-s','--species',dest = 'species', action = 'store', metavar = 'species name', help = 'Please enter the species name to choose the model, three options: H.sapiens, M.musculus, and D.melanogaster')
 parse.add_option('-t','--type',dest = 'regiontype', action = 'store', metavar = 'region type', help = 'Please enter the region type to choose the model, two options: CDS and non-CDS')
+parse.add_option('-h','--help',dest = 'help',action = 'store_false',metavar = 'help information',help = 'To show the usage information of csORF-finder')
+
 (options,args) = parse.parse_args()
+
+if (options.help):
+	parse.print_help()
+	sys.exit(0)
 
 for file in ([options.inputpath,options.inputfile,options.outputpath,options.species,options.regiontype]):
 	if not (file):
