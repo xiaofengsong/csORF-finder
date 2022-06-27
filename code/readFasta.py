5e902f705ce6a841dcd8d9651b4899b70726a39a
mengzhanggggg <84058941+mengzhanggggg@users.noreply.github.com>
2022-06-27 14:28:50 +0800

    Add files via upload

---
+++ code/readFasta.py
@@ -0,0 +1,30 @@
+##Read Fasta sequence
+import os
+import re
+from getsORFs import get_sORF
+import sys
+
+def read_fasta(file, seqtype):
+    if os.path.exists(file) == False:
+        print('Error: "' + file + '" does not exist.')
+        sys.exit(1)
+
+    with open(file) as f:
+        records = f.read()
+
+    if re.search('>', records) == None:
+        print('The input file seems not in fasta format.')
+        sys.exit(1)
+
+    records = records.split('>')[1:]
+    myFasta = []
+    for fasta in records:
+        array = fasta.split('\n')
+        name, sequence = array[0].split()[0], re.sub('[^ARNDCQEGHILKMFPSTWYV-]', '-', ''.join(array[1:]).upper())
+        myFasta.append([name, sequence])
+
+    if seqtype == 'sORF':
+        return myFasta
+    elif seqtype == 'transcript':
+        sORF_seq = get_sORF(myFasta)
+        return sORF_seq
